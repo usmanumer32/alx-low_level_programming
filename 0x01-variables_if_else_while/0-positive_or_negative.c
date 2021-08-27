975441321ba1b0216acc4ebf735979881a517eb5
usmanumer32 <88307493+usmanumer32@users.noreply.github.com>
2021-08-27 18:46:48 +0300

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -7,23 +7,19 @@
  * main function is the entry
  * point of every c program
  */
-int main(void)
-{
-	int n;
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-/* your code goes there */
-	if (n > 0)
-	{
-		printf("%d is positive\n",n);
-	}
-	if (n == 0)
-	{
-		printf("%d is zero\n",n);
-	}
-	if (n < 0)
-	{
-		printf("%d is negative\n",n);
-	}
-	return (0);
+int main() {
+    int n;
+    srand(time(0));
+    n = rand() - RAND_MAX / 2;
+    /* your code goes there */
+    if (n > 0) {
+        printf("%d is positive\n",n);
+    }
+    if (n == 0) {
+        printf("%d is zero\n",n);
+    }
+    if (n < 0) {
+        printf("%d is negative\n",n);
+    }
+    return (0);
 }
