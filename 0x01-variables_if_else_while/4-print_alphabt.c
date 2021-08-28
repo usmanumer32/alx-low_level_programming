f10a1ad00b9eec4fee50fee2a4de8255cb53a005
usmanumer32 <usmanumer32@gmail.com>
2021-08-28 22:15:06 +0300

    task 4 solution

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main - Short description (this is main function)
+* @void: no parameter
+* Return: returns 0
+*/
+int main(void)
+{
+char ch;
+
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+if (ch != 'q' || ch != 'a')
+{
+putchar(ch);
+}
+}
+printf("\n");
+return (0);
+}
