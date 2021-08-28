f135aefab55382a2dd0e550f9c21a3f2ba99aa9b
usmanumer32 <usmanumer32@gmail.com>
2021-08-28 22:25:08 +0300

    task 5 fix

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - Short description (this is main function)
+* @void: no parameter
+* Return: returns 0
+*/
+int main(void)
+{
+int i;
+
+for (i = 0; i <= 9; i++)
+{
+printf("%d", i);
+}
+printf("\n");
+return (0);
+}
