3b0f22c469c855aac1a2e45fe9a89f3c038b55b1
usmanumer32 <usmanumer32@gmail.com>
2021-08-28 21:55:25 +0300

    task 2 solution

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - Short description (this is main function)
+* @void: no parameter
+* Return: returns 0
+*/
+int main(void)
+{
+char ch;
+
+for(ch = 'a'; ch <= 'z'; ch++)
+{
+putchar(ch);
+}
+return (0);
+}
