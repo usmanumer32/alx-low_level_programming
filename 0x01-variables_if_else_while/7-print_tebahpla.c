b2772148ad4960b8a5e0573ad7879f819dfeb539
usmanumer32 <usmanumer32@gmail.com>
2021-08-28 23:01:37 +0300

    task 7 fix

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - Short description (this is main function)
+* @void: no parameter
+* Return: returns 0
+*/
+int main(void)
+{
+char ch;
+
+for (ch = 'z'; ch <= 'a'; ch--)
+{
+putchar(ch);
+}
+printf("\n");
+return (0);
+}
