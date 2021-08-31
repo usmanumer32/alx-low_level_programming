9c7ae174a638d8bc852b07df9432fe62578d3e10
usmanumer32 <usmanumer32@gmail.com>
2021-08-31 15:46:08 +0300

    task 2 fix

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,15 +1,9 @@
 #include "main.h"
 /**
-* main - check the code for ALX School students
-* 
-* Return: Always 0
+* print_alphabet_x10 - print alphabet 10 times
+*
+* Return: nothing
 */
-int main(void)
-{
-print_alphabet_x10();
-return (0);
-}
-
 void print_alphabet_x10(void)
 {
 int i;
