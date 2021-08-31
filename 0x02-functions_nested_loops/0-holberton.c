ef6d58f2e474d2b53420ab95cfcb8975f1ae9c28
usmanumer32 <usmanumer32@gmail.com>
2021-08-31 11:09:20 +0300

    task 0 solution

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* main - Short description (this is main function)
+* @void: no parameter
+* Return: returns 0
+*/
+int main(void)
+{
+char *c = "Holberton";
+int i;
+
+for (i = 0; i < 9; i++) {
+    _putchar(c[i]);
+}
+_putchar('\n');
+return (0);
+}
+
