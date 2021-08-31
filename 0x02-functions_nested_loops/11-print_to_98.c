7c8cfc98a358e5db8362d90e845282bec43bbd2f
usmanumer32 <usmanumer32@gmail.com>
2021-08-31 20:16:13 +0300

    task 11 fix

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* print_to_98 - print to 98
+* @n: int
+* Return: nothing
+*/
+void print_to_98(int n)
+{
+int i;
+
+for (i = n; i <= 98; i++)
+{
+printf("%d, ", i);
+}
+}
