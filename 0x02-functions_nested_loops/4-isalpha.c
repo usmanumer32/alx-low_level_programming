129eee7b9d3b9338946c1138edce650585a07ea8
usmanumer32 <usmanumer32@gmail.com>
2021-08-31 15:20:54 +0300

    task 4 fix

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include <ctype.h>
+#include "main.h"
+/**
+* main - check the code for ALX School students
+* 
+* Return: Always 0
+*/
+int main(void)
+{
+return (0);
+}
+
+int _isalpha(int c)
+{
+int res = isalpha(c);
+
+if (res > 0)
+{
+return (1);
+}
+return (0);
+}
