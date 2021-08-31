0a04c6b8c6895e056b762e5a16ddbe12bd7852b3
usmanumer32 <usmanumer32@gmail.com>
2021-08-31 12:19:38 +0300

    task 0 fix

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -9,10 +9,11 @@ int main(void)
 char *c = "Holberton";
 int i;
 
-for (i = 0; i < 9; i++) {
-    _putchar(c[i]);
+for (i = 0; i < 9; i++)
+{
+putchar(c[i]);
 }
-_putchar('\n');
+putchar('\n');
 return (0);
 }
 
