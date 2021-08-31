d8eea6613de9a9595962f48ee330df2a1f4ec697
usmanumer32 <usmanumer32@gmail.com>
2021-08-31 19:41:21 +0300

    task 9 fix

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+* times_table - 9 times table
+* @void: nothing
+* Return: nothing
+*/
+void times_table(void)
+{
+int i;
+int j;
+
+for (i = 0; i <= 9; i++)
+{
+for (j = 0; j <= 9; j++)
+{
+_putchar((i * j) + '0');
+_putchar(',');
+_putchar(' ');
+}
+_putchar('\n');
+}
+return;
+}
