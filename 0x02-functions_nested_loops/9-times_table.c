572f874b7309e58610b78b2f9ea0330cafe949c5
usmanumer32 <usmanumer32@gmail.com>
2021-08-31 20:04:42 +0300

    task 9 fix

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,14 +8,32 @@ void times_table(void)
 {
 int i;
 int j;
+int res;
 
 for (i = 0; i <= 9; i++)
 {
 for (j = 0; j <= 9; j++)
 {
-_putchar((i * j) + '0');
+res = i * j;
+if (j != 0)
+{
 _putchar(',');
 _putchar(' ');
+}
+if (res >= 10)
+{
+_putchar((res / 10) + '0');
+_putchar((res % 10) + '0');
+}
+else if (res < 10 && j != 0)
+{
+_putchar(' ');
+_putchar((res % 10) + '0');
+}
+else
+{
+_putchar((res % 10) + '0');
+}
 }
 _putchar('\n');
 }
