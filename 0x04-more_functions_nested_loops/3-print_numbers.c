bda5c0b2636981846652b859cba1465981b3c7ab
usmanumer32 <usmanumer32@gmail.com>
2021-09-02 10:14:03 +0300

    task 3 fix

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* print_numbers - print numbers
+* @void: nothing
+*
+* Return: nothing
+*/
+void print_numbers(void)
+{
+int i;
+
+for (i = 0; i <= 9; i++)
+{
+_putchar(i + '0');
+}
+}
