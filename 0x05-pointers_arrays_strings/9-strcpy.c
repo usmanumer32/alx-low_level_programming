4d353e17d4bb4636d2fd36eb82e70e8b89524f19
usmanumer32 <usmanumer32@gmail.com>
2021-09-06 17:43:31 +0300

    task 9 fix

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+* _strcpy - print in reverse
+* @dest: char
+* @src: char
+* Return: char
+*/
+char *_strcpy(char *dest, char *src)
+{
+strcpy(dest, src);
+return (*dest);
+}
