2c88643435e6d8011430722312bc266c9b921050
usmanumer32 <usmanumer32@gmail.com>
2021-09-06 17:00:45 +0300

    task 6 fix

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+* puts2 - print in reverse
+* @str: char
+*
+* Return: nothing
+*/
+void puts2(char *str)
+{
+int i, length;
+
+length = strlen(str);
+for (i = 0; i < length; i++)
+{
+puts(s[i]);
+}
+}
