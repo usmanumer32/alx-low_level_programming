8551cdad1e01dd01e094eca26e6ff48973671f15
usmanumer32 <usmanumer32@gmail.com>
2021-09-09 10:07:48 +0300

    task 5 fix

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include "main.h"
+/**
+* string_toupper - letters to uppercase
+* @str: char
+*
+* Return: char
+*/
+char *string_toupper(char *str)
+{
+int i, size;
+
+size = sizeof(str);
+for(i = 0; i < size; i++)
+{
+int x = isupper(str[i]);
+if (x == 0)
+{
+str[i] = toupper(str[i]);
+}
+}
+return (str);
+}
