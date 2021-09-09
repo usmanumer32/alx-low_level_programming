02704c827aa514be5ca2173446c91f6c72c98534
usmanumer32 <usmanumer32@gmail.com>
2021-09-09 08:37:23 +0300

    task 0 fix

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+* _strcat - concatenate two strings
+* @dest: char
+* @src: char
+* Return: char
+*/
+char *_strcat(char *dest, char *src)
+{
+strcat(dest, src);
+return (dest);
+}
