1bbce92e4b47d6a599dc05783cf1a8f4826fb0dc
usmanumer32 <usmanumer32@gmail.com>
2021-09-09 09:40:16 +0300

    task 4 fix

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -10,10 +10,12 @@
 void reverse_array(int *a, int n)
 {
 int i;
-int *b[n];
+int b;
 
-for (i = 0; i < n; i++)
+for (i = 0; i < n / 2; i++)
 {
-b[i] = a[n-1-i];
+b = *a[i];
+*a[i] = *a[n - 1 - i];
+*a[n - 1 - i] = b;
 }
 }
