97cb09d2a14527376f518072be3fedc01ec62d98
usmanumer32 <usmanumer32@gmail.com>
2021-09-09 11:01:03 +0300

    task 5 fix

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -8,14 +8,14 @@
 */
 char *string_toupper(char *str)
 {
-int i, size, x;
+int i, size;
 
 size = sizeof(str);
 for (i = 0; i < size - 1; i++)
 {
 if (str[i] != '\0')
 {
-x = isupper(str[i]);
+int x = isupper(str[i]);
 if (x == 0)
 {
 str[i] = toupper(str[i]);
