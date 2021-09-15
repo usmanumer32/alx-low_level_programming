2ed89db2f2858adefb3904383d3b491d03387d92
usmanumer32 <usmanumer32@gmail.com>
2021-09-15 13:09:51 +0300

    task 5 fix

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -5,15 +5,17 @@
 * @base: int
 * Return: int
 */
-int check(int i, int base){
-if (i * i == base) {
+int check(int i, int base)
+{
+if (i * i == base)
+{
 return (i);
 }
 if (i * i > base)
 {
 return (-1);
 }
-return (check(i+1, base));
+return (check(i + 1, base));
 }
 /**
 * _sqrt_recursion - return square root of a given number
