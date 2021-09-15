2c6d38e68f5c8907677759e67e21b26e4c604c27
usmanumer32 <usmanumer32@gmail.com>
2021-09-15 13:26:03 +0300

    task 6 fix

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+* check - check
+* @i: int
+* @num: int
+* Return: int
+*/
+int check(int i, int num)
+{
+if (num % i == 0 || num < 2)
+{
+return 0;
+}
+else if (i == num - 1)
+{
+return (1);
+}
+else if (num > i)
+{
+return check(i+1, num);
+}
+return 1;
+}
+/**
+* is_prime_number - prime number check
+* @n: int
+*
+* Return: int
+*/
+int is_prime_number(int n)
+{
+return (check(2, n))
+}
