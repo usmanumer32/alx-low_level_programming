e20c7fe2621e7a05193cb032ed3f66cc114c2213
usmanumer32 <usmanumer32@gmail.com>
2021-09-17 09:24:58 +0300

    task 1 fix

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+* main - main
+* @argc: int (arg count)
+* @argv: args array
+* Return: int
+*/
+int main(int argc, char *argv[])
+{
+printf("%d\n", argc - 1);
+return (0);
+}
