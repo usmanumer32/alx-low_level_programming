e266cfc5eb1da0026dea0ee455cd1058aa826136
usmanumer32 <usmanumer32@gmail.com>
2021-09-17 12:50:11 +0300

    task 4 fix

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,42 +1,42 @@
 #include <stdio.h>
-#include <ctype.h>
 #include <stdlib.h>
-#include <string.h>
+#include <ctype.h>
 #include "main.h"
 /**
-* checker - checks for valid input
-* @argc: argument count
-* @i: counter for argv[]
-* @j: counter for argv[][]
-* @argv: argument vector
-* Return: 0 on success, 1 on failure
-*/
-int checker(int argc, int i, unsigned int j, char *argv[])
-{
-for (i = 1; i <= argc; i++)
-for (j = 0; argv[i] != '\0' && j < strlen(argv[i]); j++)
-if (isdigit(argv[i][j]) == 0)
-return (1);
-return (0);
-}
-/**
-* main - adds all arguments together if they are digits.
-* @argc: argument count only accepts ints separated by spaces.
-* @argv: argument vector
-* Return: 0 on success, 1 on failure
+* main - main
+* @argc: int (arg count)
+* @argv: args array
+* Return: int
 */
 int main(int argc, char *argv[])
 {
-int result, i;
+int i, isdig, sum = 0;
+bool allNumbers = true;
 
-result = 0;
-if (checker(argc, 1, 0, argv) == 1)
+if (argc == 1)
+{
+printf("%d\n", 0);
+}
+else
+{
+for (i = 1; i < argc; i++)
+{
+isdig = isdigit(argv[i]);
+if (isdig == 0)
+{
+allNumbers = false;
+}
+else
+{
+sum = sum + atoi(argv[i]);
+}
+}
+if (!allNumbers)
 {
 printf("Error\n");
 return (1);
 }
-for (i = 1; i < argc; i++)
-result += atoi(argv[i]);
-printf("%d\n", result);
+printf("%d\n", sum);
+}
 return (0);
 }
