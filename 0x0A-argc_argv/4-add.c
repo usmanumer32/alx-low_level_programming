e6ec54b8d8e0a266c5d00f38be7061fca8f6c1b3
usmanumer32 <usmanumer32@gmail.com>
2021-09-17 09:54:29 +0300

    task 4 fix

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "main.h"
+/**
+* main - main
+* @argc: int (arg count)
+* @argv: args array
+* Return: int
+*/
+int main(int argc, char *argv[])
+{
+int i, isdig, sum = 0;
+bool allNumbers = true;
+
+if (argc == 1)
+{
+printf("%d\n", 0);
+}
+else
+{
+for (i = 1; i < argc; i++)
+{
+isdig = isdigit(argv[i]);
+if (isdig == 0)
+{
+allNumbers = false;
+}
+else
+{
+sum = sum + argv[i];
+}
+}
+if (!allNumbers)
+{
+printf("Error\n");
+return (1);
+}
+printf("%d\n", sum);
+}
+return (0);
+}
