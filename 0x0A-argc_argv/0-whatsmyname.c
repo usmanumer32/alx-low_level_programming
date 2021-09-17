53f64478bd81240e43b54ac58e85a84869b5f0c7
usmanumer32 <usmanumer32@gmail.com>
2021-09-17 09:18:18 +0300

    task 0 fix

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+* main - main
+* @argc: int (arg count)
+* @argv: args array
+* Return: int
+*/
+int main(int argc, char *argv[])
+{
+printf("%s", argv[0]);
+return (0);
+}
