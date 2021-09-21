151d853eec3c93203b286c540d2ce0a7316f903f
usmanumer32 <usmanumer32@gmail.com>
2021-09-21 08:38:00 +0300

    task 0 fix

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+* create_array - creates array of chars
+* @size: int
+* @c: char
+* Return: char
+*/
+char *create_array(unsigned int size, char c)
+{
+char *t;
+
+if (size == 0)
+{
+return (NULL);
+}
+else
+{
+t = malloc(sizeof(char) * size);
+t[0] = c;
+return (t);
+}
+}
