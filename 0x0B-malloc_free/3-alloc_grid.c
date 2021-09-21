75d462ecd2e503d6659542b5b200333555fba936
usmanumer32 <usmanumer32@gmail.com>
2021-09-21 11:18:27 +0300

    task 3 fix

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -13,7 +13,7 @@ int i, j;
 
 if (width == 0 || height == 0)
 return (NULL);
-pptr = (int *)malloc(sizeof(int) * width * height);
+pptr = (int **)malloc(sizeof(int) * width * height);
 if (pptr == NULL)
 return (NULL);
 for (i = 0; i < width; i++)
