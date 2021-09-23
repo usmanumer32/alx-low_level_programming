abb7369e11d872cd2558f9d3bfeee6bb3a08d4d2
usmanumer32 <usmanumer32@gmail.com>
2021-09-23 20:27:05 +0300

    task 2 fix

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,14 +9,11 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 int *p;
-unsigned int i;
 
 if (nmemb == 0 || size == 0)
 return (NULL);
-p = malloc(size * nmemb);
+p = calloc(size, (nmemb *sizeof(int)));
 if (p == NULL)
 return (NULL);
-for (i = 0; i < nmemb; i++)
-p[i] = 0;
 return (p);
 }
