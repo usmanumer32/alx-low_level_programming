7394be13aa8829ac4738eb8a691861ea31687eb2
usmanumer32 <usmanumer32@gmail.com>
2021-09-23 20:07:15 +0300

    task 1 fix

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,25 +11,23 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 char *p;
-int s1size, s2size, totalsize, i;
+unsigned int s1count, s2count, sizeBuffer, i;
 
 if (s1 == NULL)
 s1 = "";
 if (s2 == NULL)
 s2 = "";
-for (s1size = 0; s1[s1size]; s1size++)
+for (s1count = 0; s1[s1count]; s1count++)
 ;
-for (s2size = 0; s2[s2size]; s2size++)
+for (s2count = 0; s2[s2count]; s2count++)
 ;
-s2size > n ? (s2size = n) : (n = s2size);
-totalsize = s1size + s2size + 1;
-p = malloc(totalsize *sizeof(char));
+s2count > n ? (s2count = n) : (n = s2count);
+sizeBuffer = s1count + s2count + 1;
+p = malloc(sizeBuffer * sizeof(char));
 if (p == NULL)
 return (NULL);
-for (i = 0; i < totalsize - 1; i++)
-{
-i < s1size ? (p[i] = s1[i]) : (p[i] = s2[i - s1size]);
-}
-p[totalsize] = '\0';
+for (i = 0; i < sizeBuffer - 1; i++)
+i < s1count ? (p[i] = s1[i]) : (p[i] = s2[i - s1count]);
+p[sizeBuffer] = '\0';
 return (p);
 }
