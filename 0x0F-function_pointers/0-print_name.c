9411a9218edbb1e58adb8c4d9064124ea5c75246
usmanumer32 <usmanumer32@gmail.com>
2021-09-29 11:09:56 +0300

    task 0 fix

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+/**
+* print_name - prints a name
+* @name: char
+* @f: function
+* Return: nothing
+*/
+void print_name(char *name, void (*f)(char *))
+{
+(*f)(name);
+}
