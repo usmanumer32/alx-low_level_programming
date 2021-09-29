e545b60c9dda2829c7854a8e1f3a0a5ea07fa130
usmanumer32 <usmanumer32@gmail.com>
2021-09-29 11:47:19 +0300

    task 2 fix

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+/**
+* int_index - search for an int
+* @array: int
+* @size: int
+* @cmp: function
+* Return: int
+*/
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int i, res;
+
+if (size <= 0)
+return (-1);
+for (i = 0; i < size; i++)
+{
+res = (*cmp)(array[i]);
+if (res != 0)
+return (i);
+}
+return (-1);
+}
