57ad447c58b73b3e92a2c65b1f4aafb0c239f3ae
usmanumer32 <usmanumer32@gmail.com>
2021-09-29 12:52:39 +0300

    task 3 fix

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+/**
+* main - main
+* @argc: int (arg count)
+* @argv: args array
+* Return: int
+*/
+int main(int argc, char *argv[])
+{
+int num1, num2, res;
+
+num1 = atoi(argv[1]);
+num2 = atoi(argv[3]);
+if (argc != 4)
+{
+printf("Error\n");
+exit(98);
+}
+if (argv[2] != '+' || argv[2] != '-' || argv[2] != '*' || argv[2] != '/' || argv[2] != '%')
+{
+printf("Error\n");
+exit(99);
+}
+if ((argv[2] == '/' || argv[2] == '%') && (num2 == atoi(0))
+{
+printf("Error\n");
+exit(100);
+}
+res = (*get_op_func(argv[2]))(num1, num2);
+printf("%d\n", res);
+return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "3-calc.h"
+/**
+* op_add - adds two integers
+* @a: int
+* @b: int
+* Return: int
+*/
+int op_add(int a, int b)
+{
+return (a + b);
+}
+/**
+* op_sub - subtracts two integers
+* @a: int
+* @b: int
+* Return: int
+*/
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+/**
+* op_mul - multiplies two integers
+* @a: int
+* @b: int
+* Return: int
+*/
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+/**
+* op_div - divides two integers
+* @a: int
+* @b: int
+* Return: int
+*/
+int op_div(int a, int b)
+{
+return (a / b);
+}
+/**
+* op_mod - divide two integers returns reminder
+* @a: int
+* @b: int
+* Return: int
+*/
+int op_mod(int a, int b)
+{
+return (a % b);
+}
