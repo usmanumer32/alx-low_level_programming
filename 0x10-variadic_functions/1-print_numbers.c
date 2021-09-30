e114814fe2c623090567296dd4a9408b8f1c1dad
usmanumer32 <usmanumer32@gmail.com>
2021-09-30 10:49:37 +0300

    task 1 fix

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,25 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+/**
+* print_numbers: print numbers
+* @separator: char
+* @n: const unsigned int
+* @...: args list
+* Return: nothing
+*/
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+va_list args;
+unsigned int i;
+
+va_start(args, n);
+for (i = 0; i < n; i++)
+{
+printf("%d", va_arg(args, int));
+if (i != n - 1 && separator != NULL)
+printf("%s", separator);
+}
+va_end(args);
+printf("\n");
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,5 +4,6 @@
 #include <stddef.h>
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
 
 #endif
