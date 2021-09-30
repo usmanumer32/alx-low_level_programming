337e36373e15630f31b5e366eb8ae77d12642677
usmanumer32 <usmanumer32@gmail.com>
2021-09-30 11:22:50 +0300

    task 3 fix

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -2,9 +2,24 @@
 #define VARIADIC_FUNCTIONS_H
 
 #include <stddef.h>
+
+#define VALIDTYPESCONDITIONAL(i) (format[i] == 'c' || format[i] == 'i' || format[i] == 'f' || format[i] == 's')
+/**
+* struct validTypes - Struct validTypes
+*
+* @valid: char
+* @f: The function associated
+*/
+typedef struct validTypes
+{
+  char *valid;
+  void (*f)();
+} v_types;
+
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 
 #endif
