becc86e80e211059012e335bf8c8230e2e91c776
usmanumer32 <usmanumer32@gmail.com>
2021-10-07 10:44:37 +0300

    task 1 fix

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+* list_len - returns the number of elements of a list_t list
+* @h: singly linkd list to print
+* Return: number of elements in the list
+*/
+size_t list_len(const list_t *h)
+{
+size_t i;
+
+for (i = 1; h; i++)
+h = h->next;
+return (i);
+}
