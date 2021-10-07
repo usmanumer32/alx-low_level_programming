a824de567a4e9f3e97cb390885e3df5e9dc24c57
usmanumer32 <usmanumer32@gmail.com>
2021-10-07 20:17:44 +0300

    task 4 fix

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,6 +1,5 @@
 #include "lists.h"
 #include <stdlib.h>
-#include <stdio.h>
 #include <string.h>
 /**
 * _strlen - gets length of the string
@@ -16,34 +15,35 @@ for (i = 0; s[i]; i++)
 return (i);
 }
 /**
-* add_node - adds a new node at the beginning of a list_t list
-* @head: head of list
-* @str: string
-* Return: address of the new element
+* add_node - add new nodes to the list
+* @head: current place in the list
+* @str: string to add to the head
+* Return: pointer to current position in list
 */
 list_t *add_node(list_t **head, const char *str)
 {
 int i, len;
 char *content;
-list_t *new_node;
+list_t *new;
 
 if (str == NULL || head == NULL)
 return (NULL);
 len = _strlen(str);
-new_node = *head;
+new = *head;
 content = malloc((len + 1) * sizeof(char));
 if (content == NULL)
 return (NULL);
-content = strdup(str);
-new_node = malloc(sizeof(list_t));
-if (new_node == NULL)
+for (i = 0; str[i]; i++)
+content[i] = str[i];
+new = malloc(sizeof(list_t));
+if (new == NULL)
 {
 free(content);
 return (NULL);
 }
-new_node->str = content;
-new_node->len = len;
-new_node->next = *head;
-*head = new_node;
-return (new_node);
+new->str = content;
+new->len = len;
+new->next = *head;
+*head = new;
+return (new);
 }
